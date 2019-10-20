ee5691b17aed6880e93d3e790302769e7eeae515
Kaarthick Rameskumar <48500945+KaarthickRameskumar@users.noreply.github.com>
2019-10-20 00:00:38 -0400

    Update Controls.java

--- Controls.java
+++ Controls.java
@@ -18,39 +18,38 @@ public class Controls {
     public Controls(Camera cam){
         this.cam = cam;
     }
- 
-    
-    
-    
-        
         public final AnalogListener analogListener = new AnalogListener() {
         @Override
         public void onAnalog(String name, float value, float tpf) {
              Vector3f CameraLocation = cam.getLocation();
                 if (name.equals("forward")) {
                     if (CameraLocation.z>10) {                  
-                    CameraLocation.z-=0.2;
+                    CameraLocation.z-=0.6;
                     cam.setLocation(CameraLocation);
                     }
                 }
                 if (name.equals("back")) {
                     if (CameraLocation.z<210) {
-                    CameraLocation.z+=0.2;
+                    CameraLocation.z+=0.6;
                     cam.setLocation(CameraLocation);
                     }
                 }
                 if (name.equals("left")) {
                     if (CameraLocation.x>0) {
-                    CameraLocation.x-=0.2;
+                    CameraLocation.x-=0.6;
                     cam.setLocation(CameraLocation);
                     }
                 }
                  if (name.equals("right")) {
                     if (CameraLocation.x<150) {                     
-                    CameraLocation.x+=0.2;
+                    CameraLocation.x+=0.6;
                     cam.setLocation(CameraLocation);
                     }
                 }
+                 if(name.equals("nextTurn")){
+                     Main.nextTurn = true;
+                     System.out.println("Next turn");
+                 }
            
         }
     };
