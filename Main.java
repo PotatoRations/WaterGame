6143ab92517f53c572377c6f88d5d99cdc9652fd
Kaarthick Rameskumar <48500945+KaarthickRameskumar@users.noreply.github.com>
2019-10-20 00:03:03 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
 package mygame;
 
-import com.jme3.input.KeyInput;
-import com.jme3.input.controls.KeyTrigger;
-
 import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
-import com.jme3.light.AmbientLight;
+import com.jme3.scene.shape.Box;
+import com.jme3.light.*;
+import com.jme3.math.FastMath;
+import com.jme3.math.Plane;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector2f;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Quad;
+import com.jme3.water.SimpleWaterProcessor;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.KeyTrigger;
+
+
+import java.io.*;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -18,27 +30,31 @@ import com.jme3.light.AmbientLight;
  */
 public class Main extends SimpleApplication {
     
-        protected Geometry player;
-        public static boolean render = false;
-        public static boolean renderPop = false;
+    protected Geometry player;
+    public static boolean render = false;
+    public static boolean renderPop = false;
+    public static boolean nextTurn = true;
+    
+    Data data;
+    
 
-
+    public CameraControl ccontrol;
+    public Controls moves;
+    
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
     }
-    
-    public CameraControl ccontrol;
-    public Controls moves;
 
     @Override
-    public void simpleInitApp() {
-     
+    public void simpleInitApp() {  
+        
         AmbientLight al = new AmbientLight();
-        al.setColor(ColorRGBA.White.mult(0.1f));
+        al.setColor(ColorRGBA.White.mult(0.5f));
         rootNode.addLight(al);
         
         RenderClass.initialRender();
+        data = new Data (17,200000,200000);
         
         for(int i = 0;i<5;i++){
             for(int j = 0;j<17;j++){
@@ -63,17 +79,29 @@ public class Main extends SimpleApplication {
                     
                 }
             }
-        }  
-        flyCam.setEnabled(false);
-
+        }
         
-        //youcef start
+        
+        flyCam.setEnabled(false);
         ccontrol = new CameraControl(cam);
         moves = new Controls(cam);
         initKeys(); // load my custom keybinding
-        //youcef end
         
-        
+        DirectionalLight sun = new DirectionalLight();
+        ColorRGBA rays = new ColorRGBA(1,1,1,1);
+        sun.setColor(rays);
+        sun.setDirection(new Vector3f(-.2f,-.2f,-.8f).normalizeLocal());
+        rootNode.addLight(sun);
+                        
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {     
+        if(nextTurn){
+            nextTurn = false;
+            data.endLoop();
+            data.startLoop();
+        }
     }
     
     public void initKeys() {
@@ -82,47 +110,63 @@ public class Main extends SimpleApplication {
         inputManager.addMapping("back",   new KeyTrigger(KeyInput.KEY_S));
         inputManager.addMapping("left",  new KeyTrigger(KeyInput.KEY_A));
         inputManager.addMapping("right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("nextTurn", new KeyTrigger(KeyInput.KEY_N));
         // Add the names to the action listener.
-        inputManager.addListener(moves.analogListener, "forward", "back", "left", "right");
+        inputManager.addListener(moves.analogListener, "forward", "back", "left", "right","nextTurn");
 
-    }
-         @Override
-    public void simpleRender(RenderManager rm) {
-        if(render){
-            for(int i = 0;i<5;i++){
-            for(int j = 0;j<17;j++){
-                for(int k = 0;k<17;k++){
-                    Tile t = RenderClass.tileMap[k][i][j];
-                   
-                    
-                    Vector3f move;
-                    if(j%2 == 0 && t.type != -1){
-                        Spatial model = assetManager.loadModel(t.fileName);
-                        move = new Vector3f(j*10,i*4,k*12-6);
-                        model.move(move);
-                        rootNode.attachChild(model);
-                    }
-
-                    else if (t.type != -1){
-                        Spatial model = assetManager.loadModel(t.fileName);
-                        move = new Vector3f(j*10,i*4,k*12);
-                        model.move(move);
-                        rootNode.attachChild(model);
-                    }
-                    
-                }
-            }
-        } 
-        }
     }
 
     @Override
-    public void simpleUpdate(float tpf) {
-        //TODO: add update code
-        
-    }
+    public void simpleRender(RenderManager rm) {
+        if(render){
+            for(int i = 0;i<5;i++){
+                for(int j = 0;j<17;j++){
+                    for(int k = 0;k<17;k++){
+                        Tile t = RenderClass.tileMap[k][i][j];
 
-   
-    
+
+                        Vector3f move;
+                        if(j%2 == 0 && t.type != -1){
+                            System.out.println(t.type);
+                            System.out.println(t.fileName);
+                            Spatial model = assetManager.loadModel(t.fileName);
+                            move = new Vector3f(j*10,i*4,k*12-6);
+                            model.move(move);
+                            rootNode.attachChild(model);
+                        }
+
+                        else if (t.type != -1){
+                            Spatial model = assetManager.loadModel(t.fileName);
+                            move = new Vector3f(j*10,i*4,k*12);
+                            model.move(move);
+                            rootNode.attachChild(model);
+                        }
+
+                    }
+                }
+             } 
+          
+        }
+        /*else {
+            for(int i = 0;i<5;i++){
+                for(int j = 0;j<17;j++){
+                    for(int k = 0;k<17;k++){
+                        Tile t = RenderClass.tileMap[k][i][j];
+                        if(t.type == 2){
+                            int d = (int)(Math.random()*1-1);
+                            if(d == 0){
+                                Spatial model = assetManager.loadModel(t.fileName);
+                                Vector3f move = new Vector3f(j*10,i*4+d,k*12);
+                                model.move(move);
+                                rootNode.attachChild(model);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        */
+        render = false;
+    }
     
 }
